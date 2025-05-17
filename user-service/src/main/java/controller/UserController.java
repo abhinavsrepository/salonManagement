979607dc236103ec37aa495903ca7a26332c8dad
abhinavsrepository <abhi2510979b@gmail.com>
2025-05-18 02:15:27 +0530

    controller allded

--- user-service/src/main/java/controller/UserController.java
+++ user-service/src/main/java/controller/UserController.java
@@ -1,4 +1,20 @@
 package controller;
 
+import com.salon.modal.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+@GetMapping("/api/users")
+    public User getUser(){
+        User user = new User();
+        user.setEmail("abhinav@gmail.com");
+        user.setFullName("abhinav");
+        user.setPhone("+91 9511195090");
+        user.setRole("Customer");
+        return user;
+    }
 }
+
