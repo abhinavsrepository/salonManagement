5eb1fed9a41ab3b562a6b062baacad0ae189dbea
abhinavsrepository <abhi2510979b@gmail.com>
2025-05-18 14:27:36 +0530

    controller allded

--- user-service/src/main/java/com/salon/controller/UserController.java
+++ user-service/src/main/java/com/salon/controller/UserController.java
@@ -4,14 +4,21 @@ import com.salon.modal.User;
 import com.salon.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 @Autowired
     private UserRepository userRepository;
+@PostMapping("/api/users")
+//request body will get data from client
+public User createUser(@RequestBody User user)
+{
+    return userRepository.save(user);
 
-
+}
 
 @GetMapping("/api/users")
     public User getUser(){
