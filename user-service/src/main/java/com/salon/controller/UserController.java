3bb13db10123d6a41ea538bede408090ce44f913
abhinavsrepository <abhi2510979b@gmail.com>
2025-05-20 19:18:02 +0530

    controller allded

--- user-service/src/main/java/com/salon/controller/UserController.java
+++ user-service/src/main/java/com/salon/controller/UserController.java
@@ -2,6 +2,7 @@ package com.salon.controller;
 
 import com.salon.modal.User;
 import com.salon.repository.UserRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,7 +17,7 @@ public class UserController {
 
     @PostMapping("/api/users")
 //request body will get data from client
-    public User createUser(@RequestBody User user) {
+    public User createUser(@RequestBody @Valid User user) {
         return userRepository.save(user);
 
     }
@@ -28,17 +29,48 @@ public class UserController {
 
     }
 
-
-    public User getUserById(Long id) throws Exception {
-        Optional<User> otp=userRepository.findById(id);
-         if (otp.isPresent()){
-             return otp.get();
-         }
-         throw new Exception("user not found");
+    @GetMapping("/api/users/{userId}")
+    public User getUserById(@PathVariable("userId") Long id) throws Exception {
+        Optional<User> otp = userRepository.findById(id);
+        if (otp.isPresent()) {
+            return otp.get();
+        }
+        throw new Exception("user not found");
     }
-//    @PutMapping("/api/user/1")
-//    public User updateUser(@RequestBody User user){
-//
-//    }
+
+    //updating the user name in database;
+    @PutMapping("/api/users/{id}")
+    public User updateUser(@RequestBody User user, @PathVariable Long id) throws Exception {
+
+
+        Optional<User> otp = userRepository.findById(id);
+
+        if (otp.isEmpty()) {
+            throw new Exception("user not found by id" + id);
+        }
+        User existingUser = otp.get();
+        existingUser.setFullName((user.getFullName()
+        ));
+        existingUser.setEmail(user.getEmail());
+        existingUser.setRole(user.getRole());
+
+        return userRepository.save(existingUser);
+
+
+    }
+    @DeleteMapping("/api/users/{id}")
+    public  String deleteUserById(@PathVariable Long id) throws Exception{
+        Optional<User> otp = userRepository.findById(id);
+
+        if (otp.isEmpty()) {
+            throw new Exception("user not existe by id" + id);
+        }
+        userRepository.deleteById(otp.get().getId());
+        return "user deleted";
+
+
+
+    }
+
 }
 
--- user-service/src/main/java/com/salon/modal/User.java
+++ user-service/src/main/java/com/salon/modal/User.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 
 import java.time.LocalDateTime;
@@ -17,12 +21,30 @@ public class User {
     private Long id;
 
     private String fullName;
+    @NotBlank(message = "username is mandatory")
+    private String username;
+
+    @NotBlank(message = "email is mandatory")
+    @Email(message ="email should be valid" )
     private String email;
     private String phone;
-    private String role;
-    private LocalDateTime createdAt;//for creation
 
+    @NotBlank(message = "role is mandatory")
+    private String role;
+    @CreationTimestamp
+    private LocalDateTime createdAt;//for creation
+    @UpdateTimestamp
     private LocalDateTime updateAt;//for updation
+    @NotBlank(message = "password is mandatory")
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getEmail() {
         return email;
