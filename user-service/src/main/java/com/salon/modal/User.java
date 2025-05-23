b6c12cdc815ed789a65357b7c68cd1598e9322c3
abhinavsrepository <abhi2510979b@gmail.com>
2025-05-24 01:23:45 +0530

    service added

--- user-service/src/main/java/com/salon/modal/User.java
+++ user-service/src/main/java/com/salon/modal/User.java
@@ -6,6 +6,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -13,7 +17,10 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
 
 @Entity
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -38,80 +45,4 @@ public class User {
     @NotBlank(message = "password is mandatory")
     private String password;
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdateAt() {
-        return updateAt;
-    }
-
-    public void setUpdateAt(LocalDateTime updateAt) {
-        this.updateAt = updateAt;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    //no args constructor
-    public User() {
-    }
-
-    public User(String fullName, String email, String role, String phone, LocalDateTime createdAt, LocalDateTime updateAt) {
-        this.fullName = fullName;
-        this.email = email;
-        this.role = role;
-        this.phone = phone;
-        this.createdAt = createdAt;
-        this.updateAt = updateAt;
-    }
 }
---
+++ user-service/src/main/java/com/salon/service/UserService.java
@@ -0,0 +1,11 @@
+package com.salon.service;
+
+import com.salon.modal.User;
+
+import java.util.List;
+
+public interface UserService {
+    User creatUser(User user);
+    User getUserById(Long id);
+    List<User> getAllUsers();
+}
